8a238c44527895a166d88c4496cb524748fb4607
ZèD <imzihad@gmail.com>
2022-08-12 21:27:20 +0000

    router: initialized react router and moved theme to layout
    Signed-off-by: GitHub <noreply@github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Box, Typography } from "@mui/material";
+import { Outlet, useRoutes } from "react-router-dom";
+import ThemeLayout from "./components/Shared/ThemeLayout";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
+  // Define routes for the app
+  const routes = [
+    {
+      path: "/",
+      element: <Outlet />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+  ];
+
+  // render all routes using useRoutes hook
+  const renderRoutes = useRoutes(routes);
 
   return (
-    <Box>
-      <Typography>Material UI</Typography>
-    </Box>
+    <ThemeLayout>
+      {renderRoutes}
+    </ThemeLayout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import theme from "./configs/theme";
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme} >
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
+    <BrowserRouter>
       <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </React.StrictMode >
 );
